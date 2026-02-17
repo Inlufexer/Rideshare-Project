09277a650cc84848870db19cc0cc97cdef3158f0
Theodor Faraon <139110287+Inlufexer@users.noreply.github.com>
2026-02-17 15:00:30 -0800

    Added headers to person class

--- person.java
+++ person.java
@@ -3,10 +3,13 @@ public class person{
 
     private int endStation;
 
+
+    //constructor
     public person(int endStation){
         this.endStation = endStation;
     }
 
+    //methods
     public int getStation(){
         return endStation;
     }
