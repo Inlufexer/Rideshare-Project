dc8eefdf6030d06652f948025aafbc4b2595ee66
Theodor Faraon <139110287+Inlufexer@users.noreply.github.com>
2026-02-17 14:59:46 -0800

    Added Road class, added base functionality to person class

--- person.java
+++ person.java
@@ -1,3 +1,13 @@
 public class person{
-    
+    //fields
+
+    private int endStation;
+
+    public person(int endStation){
+        this.endStation = endStation;
+    }
+
+    public int getStation(){
+        return endStation;
+    }
 }
